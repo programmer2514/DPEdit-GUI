4006ed6bbe3785a7af5263b91f679bfa53836b70
programmer2514 <43104632+programmer2514@users.noreply.github.com>
2022-10-14 13:47:59 -0400

    Fix relaunch

--- dpedit_gui.py
+++ dpedit_gui.py
@@ -166,7 +166,7 @@ def check_for_updates(args=None):
             with open(__file__, "wb") as outfile:
                 outfile.write(response_app.content)
             messagebox.showinfo(message="DPEdit-GUI has been updated successfully!\n The application will now restart to apply changes.", title="Success")
-            Popen(__file__)
+            Popen(__file__, shell=True)
             root.destroy()
 
 
